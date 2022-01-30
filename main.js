49e30c63cec0fe8b05d9ec15b26518e7a5b50ece
Gagik Mirzoyan <gmirzoyan12@gmail.com>
2022-01-30 14:54:06 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-import { loadImages } from './functions/graphic.js';
-import { loadSounds } from './functions/sounds.js';
+import { imagesObj } from './functions/graphic.js';
+import { soundsObj } from './functions/sounds.js';
 import { start, click } from './functions/mechanics.js'
-//block 1 loading media, and getting content from html
-let gameMedia = {
-    sounds: loadSounds(),
-    images: loadImages(),
+//load media
+const gameMedia = {
+    sounds: soundsObj,
+    images: imagesObj,
     heightInput: document.getElementById("height"),
     widthInput: document.getElementById("width"),
     buttonInput: document.getElementById("button"),
     statusBar: document.getElementById("statusBar"),
     flagsBar: document.getElementById("flagAmount"),
     timerBar: document.getElementById("timer"),
-    canvas: document.getElementById("canvas"),
-    timer: 0
+    canvas: document.getElementById("canvas")
 }
 
-//block 2 game's main params
-let gameObj = {
+//params
+const gameObj = {
     contex: canvas.getContext("2d"),
-    board: [],
-    viewMap: [],
+    board: new Array(2),
+    viewMap: new Array(2),
     width: 0,
     height: 0,
     flags: 0,
     timerId: 0,
+    timer: 0,
     state: -1,
     openedCells: 0,
-    mousePosition: [,],
-    clickPosition: [,],
+    mousePosition: new Array(2),
+    clickPosition: new Array(2),
     clickState: 0
 }
 
